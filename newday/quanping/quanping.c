edd807842fceca3273c2f1583890c99545b8e5a8
zhk <zhk@ubuntu.(none)>
2015-05-27 18:08:05 -0700

    fullscreen

--- newday/quanping/quanping.c
+++ newday/quanping/quanping.c
@@ -35,18 +35,15 @@ int main(int argc, char *argv[])
 	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
 	//gtk_window_set_resizable(GTK_WINDOW(window),FALSE);
-   	gtk_window_set_default_size(GTK_WINDOW(window), width, height);
-	//gtk_window_fullscreen(GTK_WINDOW(window));
+   	//gtk_window_set_default_size(GTK_WINDOW(window), width, height);
+	gtk_window_fullscreen(GTK_WINDOW(window));
 	//gtk_window_maximize(GTK_WINDOW (window));
 	gtk_window_set_decorated(GTK_WINDOW(window), FALSE); /* hide the title bar and the boder */ 
 
-	//fixed = gtk_fixed_new();
-  	//gtk_container_add(GTK_CONTAINER(window), fixed);
-	image = gtk_image_new_from_file ("./2.jpg");
+	image = gtk_image_new_from_file ("./6.jpg");
 	pixbuf = gtk_image_get_pixbuf (GTK_IMAGE(image));
-	pixbuf = gdk_pixbuf_scale_simple(pixbuf, gdk_screen_get_width(screen) - 1, gdk_screen_get_height(screen) - 1,GDK_INTERP_BILINEAR);
+	pixbuf = gdk_pixbuf_scale_simple(pixbuf, gdk_screen_get_width(screen), gdk_screen_get_height(screen),GDK_INTERP_BILINEAR);
     	gtk_image_set_from_pixbuf(GTK_IMAGE(image), pixbuf);
-	//gtk_fixed_put(GTK_FIXED(fixed), image, 0, 0);
 	gtk_container_add(GTK_CONTAINER(window),image);
 	gtk_widget_show_all(window);
 	/* add key event for quit */
